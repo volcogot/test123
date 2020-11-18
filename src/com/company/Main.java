b4eaa8215d94401cff43899cb6b60eac3780dcc6
volcogot <volcogot@mail.ru>
2020-11-18 22:18:16 +0300

    Fourth commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Second commit");
         System.out.println("Next Commit");
+        System.out.println("Afanasiy");
     }
 }
